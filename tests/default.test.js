77056949981b15dc0ab28c6ab9f41a613f6308c4
postor <postor@gmail.com>
2018-10-14 20:49:47 +0800

    优化清理策略

--- tests/default.test.js
+++ tests/default.test.js
@@ -3,6 +3,13 @@ const Differencify = require('differencify')
 const { baseUrl, urls } = require('./config')
 const launchConfig = require('../getLaunchConfig')()
 
+let toClean = []
+
+afterEach(() => {
+  toClean.forEach(x => x())
+  toClean = []
+})
+
 describe('url-', () => {
   urls.forEach(async (url, i) => {
     it(url.replace(/\W/g, ''), async () => {
@@ -11,13 +18,15 @@ describe('url-', () => {
       const target = differencify.init({ testName: `t${i}`, chain: false });
       const browser = await target.launch(launchConfig)
       const page = await browser.newPage()
+      toClean.push(async () => {
+        await page.close()
+        await browser.close()
+        await differencify.cleanup()
+      })
       await page.goto(url)
       const image = await page.screenshot({ fullPage: true })
       const result = await target.toMatchSnapshot(image)
       expect(result).toBe(true)
-      await page.close()
-      await browser.close()
-      await differencify.cleanup()
     }, 60000)
   })
 })
